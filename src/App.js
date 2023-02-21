f47abc752f89d65cbf1ef312e2b93e0775de0e76
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-02-21 13:39:26 +0300

    Компоненты с классом

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   const [value, setValue] = useState('Текст в инпуте')
 
   return (
     <div className="App">
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
