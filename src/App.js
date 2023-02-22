d7dc8ae522f80cec68b4171e6e258fc5f2f135bd
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-02-23 00:16:24 +0300

    UI библиотека, хук useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import './styles/App.css'
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'Javascript', body: 'Description'},
-    {id: 3, title: 'Javascript', body: 'Description'},
-  ])
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python', body: 'Description'},
-    {id: 2, title: 'Python', body: 'Description'},
-    {id: 3, title: 'Python', body: 'Description'},
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript', body: 'Description' },
+    { id: 3, title: 'Javascript', body: 'Description' },
   ])
 
+  const [title, setTitle] = useState('Hello')
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <PostList posts={posts} title="Посты про JS"/>
-      <PostList posts={posts2} title="Посты про Python"/>
+    <div className='App'>
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type='text'
+          placeholder='Название поста'
+        />
+        {/* Неуправляемый\Неконтролируемый компонент */}
+        <MyInput
+          ref={bodyInputRef}
+          type='text'
+          placeholder='Описание поста'
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Посты про JS' />
+
     </div>
   );
 }
